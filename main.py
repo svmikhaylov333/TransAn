cf2e8d061d4f7b69507903a48457b678c8a7acf9
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-07-26 01:15:42 +0300

    add readme!!! перенес main.py в корень, почему-то он не запускался через poetry run python src/main.py. не мог найти src-разобраться

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import json
-import sys
-from pathlib import Path
 
 from src.views import main_page, save_response_to_json
 
-sys.path.insert(0, str(Path(__file__).parent))
+# import sys
+# from pathlib import Path
+
+
+# sys.path.insert(0, str(Path(__file__)))
 
 
 def main() -> None:
     """Главная страница"""
-    #date = "2025-07-27 00:00:00"
+    # date = "2025-07-27 00:00:00"
     date = "2021-12-31 12:00:00"
-    excel_path = Path(__file__).resolve().parent.parent / "data/operations.xlsx"
+    excel_path = "data/operations.xlsx"
 
     result = main_page(date, str(excel_path))
     print(json.dumps(result, ensure_ascii=False, indent=2))
