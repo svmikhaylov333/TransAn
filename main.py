d6e0430ef96b92ab52043c8252bd44b0cbbbc17d
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-07-26 02:42:01 +0300

    1. прогон mypy isort, black, flake8, корректировка по результатам

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import json
 
-from src.views import main_page, save_response_to_json
 from src.services import get_cashback_categories
+from src.views import main_page, save_response_to_json
+
 # import sys
 # from pathlib import Path
 
 
 # sys.path.insert(0, str(Path(__file__)))
 excel_path = "data/operations.xlsx"
+
+
 # ====================
 # Страницы
 # ====================
@@ -16,7 +19,7 @@ def main() -> None:
     """Главная страница"""
     # date = "2025-07-27 00:00:00"
     date = "2021-12-31 12:00:00"
-    #excel_path = "data/operations.xlsx"
+    # excel_path = "data/operations.xlsx"
 
     result = main_page(date, str(excel_path))
     print(json.dumps(result, ensure_ascii=False, indent=2))
