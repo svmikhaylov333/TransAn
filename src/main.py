9668968700631c5c133141d2fe605e3a1c5e5488
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-07-25 23:47:42 +0300

    корректировка main.py - путь к excel файлу

--- src/main.py
+++ src/main.py
@@ -9,8 +9,11 @@ sys.path.insert(0, str(Path(__file__).parent))
 
 def main() -> None:
     """Главная страница"""
-    date = "2025-07-27 00:00:00"
-    result = main_page(date)
+    #date = "2025-07-27 00:00:00"
+    date = "2021-12-31 12:00:00"
+    excel_path = Path(__file__).resolve().parent.parent / "data/operations.xlsx"
+
+    result = main_page(date, str(excel_path))
     print(json.dumps(result, ensure_ascii=False, indent=2))
     save_response_to_json(result)
 
